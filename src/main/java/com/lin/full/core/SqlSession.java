4b28fb66f5b1700743995aeda76e6caad07a709a
lkmc2 <lkmc2@163.com>
2018-09-19 22:26:46 +0800

    添加SQL会话

---
+++ src/main/java/com/lin/full/core/SqlSession.java
@@ -0,0 +1,26 @@
+package com.lin.full.core;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * @author lkmc2
+ * @date 2018/9/19
+ * @description SQL会话
+ */
+public class SqlSession {
+
+    /**
+     * 获取Mapper对象
+     * @param clazz Mapper接口类
+     * @return Mapper对象代理
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T getMapper(Class<T> clazz) {
+        return (T) Proxy.newProxyInstance(
+                clazz.getClassLoader(),
+                new Class[]{ clazz },
+                new SqlExecuteHandler()
+        );
+    }
+
+}
